ee32dff52941e76a336c4dd8755b55dbffb8adc1
patrickpollet <pp@patrickpollet.net>
2011-12-13 15:08:00 +0100

    début des deux scripts

---
+++ cli/mahara_sync_users.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage mahara-sync-ldap
+ * @author     Patrick Pollet <pp@patrickpollet.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006-2011 Catalyst IT Ltd http://catalyst.net.nz
+ * @copyright  (C) 2011 INSA de Lyon France
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ *    Moodle - Modular Object-Oriented Dynamic Learning Environment
+ *             http://moodle.com
+ *
+ *    Copyright (C) 2001-3001 Martin Dougiamas        http://dougiamas.com
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details:
+ *
+ *             http://www.gnu.org/copyleft/gpl.html
+ */
+
+/**
+ * The purpose of this CLI script is to be run as a cron job to synchronize Mahara' users
+ * with users defined on a LDAP server
+ *
+ * This script requires as a single parameter the name of the target institution
+ * in which users will be created/updated.
+ * An instance of LDAP or CAS auth plugin MUST have been added to this institution
+ * for this script to retrieve LDAP parameters
+ * It is possible to run this script for several institutions
+ *
+ * For the synchronisation of group membership , this script MUST be run before
+ * the mahara_sync_groups script
+ *
+ * This script is strongly inspired of synching Moodle's users with LDAP 
+ */
+
+
