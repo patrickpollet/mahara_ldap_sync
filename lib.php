3c4e1b3fbf489ddb409c3c7461f1a5f2d2fc9fa1
patrickpollet <pp@patrickpollet.net>
2013-07-11 12:22:53 +0200

    fixed a PHP notice when using restricted list of values to use in synchying Mahara groups with some LDAP attribute

--- lib.php
+++ lib.php
@@ -1,10 +1,47 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: root
- * Date: 14/12/11
- * Time: 14:12
- * To change this template use File | Settings | File Templates.
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage mahara-sync-ldap
+ * @author     Patrick Pollet <pp@patrickpollet.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006-2011 Catalyst IT Ltd http://catalyst.net.nz
+ * @copyright  (C) 2011 INSA de Lyon France
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ *    Moodle - Modular Object-Oriented Dynamic Learning Environment
+ *             http://moodle.com
+ *
+ *    Copyright (C) 2001-3001 Martin Dougiamas        http://dougiamas.com
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details:
+ *
+ *             http://www.gnu.org/copyleft/gpl.html
  */
 
 
@@ -46,10 +83,10 @@ class GAAuthLdap extends AuthLdap {
 		
 		// restricted list of values to use in synchying Mahara's groups with some LDAP attribute
 		if (!empty($CFG->group_synching_ldap_attribute_values))
-			$this->config->group_synching_ldap_attribute_values=
+			$this->config['group_synching_ldap_attribute_values']=
 			  explode(',',$CFG->group_synching_ldap_attribute_values);
 		else	
-		    $this->config->group_synching_ldap_attribute_values=array();
+		    $this->config['group_synching_ldap_attribute_values']=array();
 		
 
 	}
